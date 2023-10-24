c66151164e06604ab4fb4fe88228568358bba540
yaochoaB2023 <ochoabautistayeisonalberto@gmail.com>
2023-10-24 14:35:38 -0500

    rutas de los empleados

---
+++ servidor/src/routes/employees.routes.js
@@ -0,0 +1,12 @@
+import { Router } from "express";
+import { getEmployee, getEmployees, createEmployee, updateEmployee, deleteEmployee } from '../controllers/employees.controller.js';
+
+const router = Router()
+
+router.get( "/employee", getEmployees );
+router.get( "/employee/id", getEmployee );
+router.post( "/employee", createEmployee );
+router.put( "/employee/id", updateEmployee );
+router.delete( "/employee/id", deleteEmployee );
+
+export default router;
